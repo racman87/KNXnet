c022a184b7ccfa1428a383b4060d234c31ee4b59
manon <racine.mano@gmail.com>
2017-10-21 10:11:42 -0700

    -add read function

--- KNX.py
+++ KNX.py
@@ -154,8 +154,85 @@ class connectionKNX:
         data_endpoint = ('0.0.0.0', 0)
         control_endpoint = ('0.0.0.0', 0)
 
+        #   -----------------------------------
+        #   -> (1) Sending Connection request
+        #   -----------------------------------
 
-        # TODO
+        conn_resp_object = self.connectionRequest()
+
+        # <- Retrieving channel_id & status from Connection response
+        conn_channel_id = conn_resp_object.channel_id
+        conn_status = conn_resp_object.status
+
+        print('Channel ID: ', conn_channel_id)
+        print('Channel status: ', conn_status)
+        print('-----------------------------------')
+
+        #   -----------------------------------
+        #   -> (2) Sending Connection State request
+        #   -----------------------------------
+
+        state_resp_object = self.connectionStateRequest(conn_channel_id)
+
+        # <- Retrieving channel_id & status from Connection State response
+        state_channel_id = state_resp_object.channel_id
+        state_status = state_resp_object.status
+
+        print('Channel ID: ', state_channel_id)
+        print('Channel status: ', state_status)
+        print('-----------------------------------')
+
+        #   -----------------------------------
+        #   -> (3) Tunneling request (read)
+        #   -----------------------------------
+        tunneling_req = knxnet.create_frame(knxnet.ServiceTypeDescriptor.TUNNELLING_REQUEST,
+                                         dest_group_addr,
+                                         conn_channel_id,
+                                         0,
+                                         1,
+                                         0)
+        self.sock.sendto(tunneling_req.frame, (self.gateway_ip, self.gateway_port))
+
+        print('OK1')
+
+        # <- Tunneling ack
+        data_recv, addr = self.sock.recvfrom(1024)
+        tunnel_resp_object = knxnet.decode_frame(data_recv)
+
+        print('OK2')
+        # <- Read tunnelling request
+        data_recv, addr = self.sock.recvfrom(1024)
+        tunnel_resp_object = knxnet.decode_frame(data_recv)
+
+        print('OK3')
+        # <- Send Tunneling ack
+        tunnel_ack = knxnet.create_frame(knxnet.ServiceTypeDescriptor.TUNNELLING_ACK,
+                                         conn_channel_id,
+                                         0,
+                                         tunnel_resp_object.sequence_counter)
+        self.sock.sendto(tunnel_ack.frame, (self.gateway_ip, self.gateway_port))
+
+        # <- Read Status
+        data_recv, addr = self.sock.recvfrom(1024)
+        tunnel_resp_object = knxnet.decode_frame(data_recv)
+
+
+        #   -----------------------------------
+        #   -> (5) Disconnect request
+        #   -----------------------------------
+
+        disconnect_resp_object = self.disconnect_request(conn_channel_id)
+        disconnect_resp_object = self.disconnect_request(conn_channel_id)
+
+        # <- Retrieving data from disconnect request
+        disconnect_channel_id = disconnect_resp_object.channel_id
+        disconnect_status = disconnect_resp_object.status
+        print('Channel ID: ', disconnect_channel_id)
+        print('Channel status: ', disconnect_status)
+
+        print('-----------------------------------')
+
+        return tunnel_resp_object.data
 
 
 def main(argv):
@@ -164,6 +241,8 @@ def main(argv):
     apci = argv[2]
     grp_add = argv[3]
 
+    print(grp_add[0])
+
     print('First arg is "', argv[0])
     print('Second arg is "', argv[1])
     print('Third arg is "', argv[2])
@@ -171,8 +250,15 @@ def main(argv):
 
     dest_addr_group = knxnet.GroupAddress.from_str(grp_add)
 
+
     c1 = connectionKNX("127.0.0.1", 3671)
-    c1.send_data(data, size, dest_addr_group)
+
+    if grp_add[0] < '4' :
+        c1.send_data(data, size, dest_addr_group)
+
+    else:
+        data=c1.read_data(dest_addr_group)
+        print('The value is : "',data)
 
 
 if __name__ == "__main__":
